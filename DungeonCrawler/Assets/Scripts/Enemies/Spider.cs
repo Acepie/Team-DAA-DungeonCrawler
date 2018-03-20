1c760032c57f23733f3f66d1bd622f765d314a59
Ameen Radwan <therespie@gmail.com>
2018-03-19 20:31:53 -0400

    moved drops to any enemy

--- DungeonCrawler/Assets/Scripts/Enemies/Spider.cs
+++ DungeonCrawler/Assets/Scripts/Enemies/Spider.cs
@@ -4,16 +4,5 @@ using UnityEngine;
 
 public class Spider : AbstractEnemy
 {
-    public GameObject dropItem;
-    [Range(0, 1)]
-    public float dropRate;
 
-    public override void OnDeath()
-    {
-        if (Random.value > 0.5 && dropItem) {
-            GameObject item = Instantiate(dropItem, transform.position, Quaternion.identity);
-        }
-
-        Destroy(this.gameObject);
-    }
 }
